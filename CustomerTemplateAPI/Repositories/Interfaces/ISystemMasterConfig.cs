d86081af1793c281fcc0de151f24c91e4c7dad51
Hikari <hoangvuong19991964@gmail.com>
2021-05-19 22:27:33 +0700

    implement master config controller

---
+++ CustomerTemplateAPI/Repositories/Interfaces/ISystemMasterConfig.cs
@@ -0,0 +1,16 @@
+﻿using CustomerTemplateAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerTemplateAPI.Repositories.Interfaces
+{
+    public interface ISystemMasterConfigRepository
+    {
+        IEnumerable<SystemMasterConfig> FindAll();
+        Task<SystemMasterConfig> FindById(int id);
+        Task<SystemMasterConfig> Modify(SystemMasterConfig updatedConfig);
+        Task<SystemMasterConfig> FindByName(string configName);
+    }
+}
---
+++ CustomerTemplateAPI/Repositories/SystemMasterConfigRepository.cs
@@ -0,0 +1,45 @@
+﻿using CustomerTemplateAPI.Data;
+using CustomerTemplateAPI.Models;
+using CustomerTemplateAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerTemplateAPI.Repositories
+{
+    public class SystemMasterConfigRepository : GenericRepository<SystemMasterConfig>, ISystemMasterConfigRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public SystemMasterConfigRepository(ApplicationDbContext context) : base(context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IEnumerable<SystemMasterConfig> FindAll()
+        {
+            return GetAll().AsQueryable();
+        }
+
+        public async Task<SystemMasterConfig> FindById(int id)
+        {
+            object[] parameters = new object[1];
+            parameters[0] = id;
+
+            return await GetItemById(parameters);
+        }
+
+        public async Task<SystemMasterConfig> Modify(SystemMasterConfig updatedConfig)
+        {
+            updatedConfig.ModifiedDate = DateTime.UtcNow;
+            return await Update(updatedConfig);
+        }
+
+        public async Task<SystemMasterConfig> FindByName(string configName)
+        {
+            return await GetAll().AsQueryable().FirstOrDefaultAsync(t => t.ConfigName == configName);
+        }
+    }
+}
