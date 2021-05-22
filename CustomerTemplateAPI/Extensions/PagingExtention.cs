168e20bd331116954437f0c0ca687b106213070e
Hikari <hoangvuong19991964@gmail.com>
2021-05-22 10:08:44 +0700

    adding paging extension

---
+++ CustomerTemplateAPI/Extensions/PagingExtention.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerTemplateAPI.Extensions
+{
+    public static class PagingExtention
+    {
+        public static IQueryable<TSource> Paging<TSource>(this IQueryable<TSource> source, int page, int pageSize)
+        {
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        //used by LINQ
+        public static IEnumerable<TSource> Paging<TSource>(this IEnumerable<TSource> source, int page, int pageSize)
+        {
+            return source.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
