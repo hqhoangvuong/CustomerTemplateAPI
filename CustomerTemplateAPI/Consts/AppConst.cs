c3e3533b469c9129a0eff8024872d4521d507dd2
Hikari <hoangvuong19991964@gmail.com>
2021-05-22 14:33:11 +0700

    add AppConst to store application's constant

---
+++ CustomerTemplateAPI/Consts/AppConst.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerTemplateAPI.Consts
+{
+    public enum UIComponents
+    {
+        TextField,
+        Checkbox,
+        DateTimePicker,
+        Radio,
+        Switch,
+        Select,
+        List
+    };
+}
